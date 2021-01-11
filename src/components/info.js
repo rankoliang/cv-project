5b7a4f0b7a3c8351a153a8bbb7218bae7cef5a5f
Ranko Liang <rankoliang@gmail.com>
2021-01-10 22:30:03 -0500

    Rename Info to InfoDisplay

--- src/components/info.js
+++ src/components/info.js
@@ -1,29 +1,41 @@
 import { Component } from 'react';
+import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
 import styled from 'styled-components';
 
-const StyledInfo = styled.div`
+const StyledInfoDisplay = styled.div`
   text-align: center;
 
   h1 {
     border-bottom: 1px solid black;
     margin: 0;
   }
+`;
+
+const StyledContact = styled.address`
+  display: flex;
+  justify-content: center;
+  margin: 0.25em;
 
   a {
     color: inherit;
     font-style: normal;
     text-decoration: none;
-  }
-`;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-const StyledContact = styled.address`
-  & > *:not(:last-child):after {
+    * {
+      margin-right: 0.2em;
+    }
+  }
+
+  *:not(:last-child):after {
     content: '|';
     margin: 0 0.25em;
   }
 `;
 
-class Info extends Component {
+class InfoDisplay extends Component {
   constructor(props) {
     super(props);
 
@@ -41,10 +53,10 @@ class Info extends Component {
     const { name, email, phone } = this.state;
 
     return (
-      <StyledInfo>
+      <StyledInfoDisplay>
         <h1>{name}</h1>
         <Contact email={email} phone={phone} />
-      </StyledInfo>
+      </StyledInfoDisplay>
     );
   }
 }
@@ -59,11 +71,17 @@ class Contact extends Component {
 
     return (
       <StyledContact>
-        <a href={`mailto:${email}`}>{email}</a>
-        <a href={`tel:${phone}`}>{phone}</a>
+        <a href={`mailto:${email}`}>
+          <AiOutlineMail />
+          {email}
+        </a>
+        <a href={`tel:${phone}`}>
+          <AiOutlinePhone />
+          {phone}
+        </a>
       </StyledContact>
     );
   }
 }
 
-export default Info;
+export default InfoDisplay;
